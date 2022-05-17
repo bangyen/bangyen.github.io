50d4c29b26342bbf6390adba4e7bc1ad11cfc322
Bangyen Pham <bangyen99@gmail.com>
2022-05-16 21:39:56 -0400

    added interpreter dropdown menu

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,33 +1,79 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {withRouter, Link} from 'react-router-dom';
 
-export default function Home() {
-    document.title = 'Home';
-    return (
-        <header className='App-header'>
-            <h1 className='logo'>
-                Bangyen
-            </h1>
-            <div>
-                <Link to='/back'>
+class Home extends React.Component {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            select: false,
+            value: 'back'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        document.title = 'Home';
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        this.props.history.push(`/${this.state.value}`);
+    }
+
+    render() {
+        let select;
+
+        if (this.state.select)
+            select = (
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <select value={this.state.value} onChange={this.handleChange}>
+                            <option value="back">Back</option>
+                            <option value="suffolk">Suffolk</option>
+                        </select>
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>);
+        else
+            select = (null);
+
+        return (
+            <header className='App-header'>
+                <h1 className='logo'>
+                    Bangyen
+                </h1>
+                <div>
                     <button className='custom'
-                            type='button'>
+                            type='button'
+                            onClick={() => this.setState({
+                                select: true})}>
                         Interpreter
                     </button>
-                </Link>
-                <Link to='/snake'>
-                    <button className='custom'
-                            type='button'>
-                        Snake
-                    </button>
-                </Link>
-                <form action='https://github.com/bangyen'
-                        style={{display: 'inline-block'}}>
-                    <input type='submit'
-                        value='GitHub'
-                        className='custom' />
-                </form>
-            </div>
-        </header>
-    );
+                    <Link to='/snake'>
+                        <button className='custom'
+                                type='button'>
+                            Snake
+                        </button>
+                    </Link>
+                    <form action='https://github.com/bangyen'
+                            style={{display: 'inline-block'}}>
+                        <input type='submit'
+                            value='GitHub'
+                            className='custom' />
+                    </form>
+                </div>
+                <div>
+                    {select}
+                </div>
+            </header>
+        );
+    }
 }
+
+export default withRouter(Home);
