951479f4e20da09f46f2800f9aadf4ea0ca120e9
Bangyen Pham <bangyen99@gmail.com>
2022-05-16 21:01:38 -0400

    moved interpreters

---
+++ src/Components/Interpreters/Back.js
@@ -0,0 +1,67 @@
+import Grid from '../Grid';
+
+function run(code) {
+    let row = code.length;
+    let col = code[0].length;
+    let arr = [{
+        end: false,
+        pos: [0, 0],
+        vel: [0, 1],
+        tape: [0],
+        cell: 0
+    }];
+    let ind = 0;
+
+    return function(back = false) {
+        let state = arr[arr.length - 1];
+
+        if (back && ind)
+            ind -= 1;
+        else if (!back && !state.end)
+            ind += 1;
+
+        if (ind < arr.length)
+            return arr[ind];
+
+        let {tape, cell, end} = state;
+        let [x, y] = state.pos;
+        let [a, b] = state.vel;
+        let c = code[x][y];
+        tape = [...tape];
+
+        if (c === '\\') {
+            [a, b] = [b, a];
+        } else if (c === '/') {
+            [a, b] = [-b, -a];
+        } else if (c === '<' && cell) {
+            cell -= 1;
+        } else if (c === '>') {
+            cell += 1;
+            if (cell === tape.length)
+                tape.push(0);
+        } else if (c === '-') {
+            tape[cell] ^= 1;
+        } else if (c === '+' && !tape[cell]) {
+            [x, y] = [x + a, y + b];
+        } else if (c === '*') {
+            end = true;
+        }
+
+        x = (x + a + row) % row;
+        y = (y + b + col) % col;
+        state = {
+            pos: [x, y],
+            vel: [a, b],
+            tape: tape,
+            cell: cell,
+            end: end
+        };
+        arr.push(state);
+
+        return state;
+    };
+}
+
+export default Back() {
+    return <Grid run={run} />;
+}
---
+++ src/Components/Interpreters/Suffolk.js
@@ -0,0 +1,81 @@
+import Interpret from '../Interpret';
+
+function run(input) {
+    let code = '';
+
+    for (let c of input)
+        if ('><!,.'.includes(c))
+            code += c;
+
+    let len = code.length;
+    let arr = [{
+        tape: [0],
+        acc: 0,
+        ind: 0,
+        ptr: 0,
+        out: '',
+        end: false
+    }];
+
+    return function(back = false) {
+        let state = arr[arr.length - 1];
+        let ind = state.ind;
+        let c = code[ind % len];
+
+        if (back && ind)
+            ind -= 1;
+        else if (!back && !state.end)
+            ind += 1;
+
+        if (ind < arr.length)
+            return arr[ind];
+
+        let {tape, acc, ptr, out, end}
+            = state;
+        tape = [...tape];
+
+        if (c === '>') {
+            ptr += 1;
+            if (ptr === tape.length)
+                tape.push(0);
+        } else if (c === '<') {
+            acc += tape[ptr];
+            ptr = 0;
+        } else if (c === '!') {
+            tape[ptr] -= acc - 1;
+
+            if (tape[ptr] < 0)
+                tape[ptr] = 0;
+
+            acc = 0;
+            ptr = 0;
+        } else if (c === ',') {
+            let inp = '';
+
+            while (!inp.length)
+                inp = prompt('Input: ');
+
+            acc = inp.charCodeAt(0);
+        } else if (c === '.') {
+            if (acc > 0)
+                out += String
+                    .fromCharCode(acc - 1);
+        }
+
+        state = {
+            tape: tape,
+            acc: acc,
+            ind: ind,
+            ptr: ptr,
+            out: out,
+            end: end
+        };
+
+        arr.push(state);
+        return state;
+    };
+}
+
+export default Suffolk() {
+    return <Interpret run={run} />;
+}
