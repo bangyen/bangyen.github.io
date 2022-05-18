f037084551b53306e0924a2f519fa8549224e8f5
Bangyen Pham <bangyen99@gmail.com>
2022-05-18 09:30:57 -0400

    created wrapper function

--- src/Components/Interpreters/StunStep.js
+++ src/Components/Interpreters/StunStep.js
@@ -1,85 +1,90 @@
 import TextBox from '../TextBox';
 
-let obj = {
+function outer(obj) {
+    function run(input) {
+        let code = '';
+
+        for (let c of input)
+            if ('+-><'.includes(c))
+                code += c;
+
+        let len = code.length;
+        let arr = [obj];
+        let num = 0;
+
+        function inner(back = false) {
+            let state = arr[arr.length - 1];
+            let {tape, ptr, end} = state;
+            let c = code[num % len];
+
+            if (back) {
+                if (num)
+                    num -= 1;
+            } else {
+                num += 1;
+
+                if (num % len === 0) {
+                    end = true;
+                } else if (state.end) {
+                    if (tape[ptr]) {
+                        end = false;
+                    } else {
+                        arr = [obj];
+                        num = 0;
+                    }
+                }
+            }
+
+            if (num < arr.length)
+                return arr[num];
+
+            let ind = num % len;
+            tape = [...tape];
+
+            if (c === '+') {
+                tape[ptr] += 1;
+            } else if (tape[ptr]) {
+                if (c === '-') {
+                    tape[ptr] -= 1;
+                } else if (c === '>') {
+                    ptr += 1;
+
+                    if (ptr === tape.length)
+                        tape.push(1);
+                } else if (ptr) {
+                    ptr -= 1;
+                }
+            }
+
+            state = {
+                tape: tape,
+                ind: ind,
+                ptr: ptr,
+                end: end
+            };
+
+            arr.push(state);
+            return state;
+        };
+
+        return {
+            run: inner,
+            code: code
+        };
+    }
+
+    return run;
+}
+
+export default function StunStep() {
+    let obj = {
         tape: [1],
         ind: 0,
         ptr: 0,
         end: false
     };
 
-function run(input) {
-    let code = '';
-
-    for (let c of input)
-        if ('+-><'.includes(c))
-            code += c;
-
-    let len = code.length;
-    let arr = [obj];
-    let num = 0;
-
-    function inner(back = false) {
-        let state = arr[arr.length - 1];
-        let {tape, ptr, end} = state;
-        let c = code[num % len];
-
-        if (back) {
-            if (num)
-                num -= 1;
-        } else {
-            num += 1;
-
-            if (num % len === 0) {
-                end = true;
-            } else if (state.end) {
-                if (tape[ptr]) {
-                    end = false;
-                } else {
-                    arr = [obj];
-                    num = 0;
-                }
-            }
-        }
-
-        if (num < arr.length)
-            return arr[num];
-
-        let ind = num % len;
-        tape = [...tape];
-
-        if (c === '+') {
-            tape[ptr] += 1;
-        } else if (tape[ptr]) {
-            if (c === '-') {
-                tape[ptr] -= 1;
-            } else if (c === '>') {
-                ptr += 1;
-
-                if (ptr === tape.length)
-                    tape.push(1);
-            } else if (ptr) {
-                ptr -= 1;
-            }
-        }
-
-        state = {
-            tape: tape,
-            ind: ind,
-            ptr: ptr,
-            end: end
-        };
-
-        arr.push(state);
-        return state;
-    };
-
-    return {
-        run: inner,
-        code: code
-    };
-}
-
-export default function StunStep() {
+    let run = outer(obj);
     return <TextBox
         name='Stun Step'
         link='Stun_Step'
--- src/Components/Interpreters/Suffolk.js
+++ src/Components/Interpreters/Suffolk.js
@@ -1,6 +1,93 @@
 import TextBox from '../TextBox';
 
-let obj = {
+function outer(obj) {
+    function run(input) {
+        let code = '';
+
+        for (let c of input)
+            if ('><!,.'.includes(c))
+                code += c;
+
+        let len = code.length;
+        let arr = [obj];
+        let num = 0;
+
+        function inner(back = false) {
+            let state = arr[arr.length - 1];
+            let c = code[num % len];
+            let {end} = state;
+
+            if (back) {
+                if (num)
+                    num -= 1;
+            } else {
+                num += 1;
+
+                if (num % len === 0)
+                    end = true;
+                else if (state.end)
+                    end = false;
+            }
+
+            if (num < arr.length)
+                return arr[num];
+
+            let {tape, acc, ptr, out} = state;
+            let ind = num % len;
+            tape = [...tape];
+
+            if (c === '>') {
+                ptr += 1;
+                if (ptr === tape.length)
+                    tape.push(0);
+            } else if (c === '<') {
+                acc += tape[ptr];
+                ptr = 0;
+            } else if (c === '!') {
+                tape[ptr] -= acc - 1;
+
+                if (tape[ptr] < 0)
+                    tape[ptr] = 0;
+
+                acc = 0;
+                ptr = 0;
+            } else if (c === ',') {
+                let inp = '';
+
+                while (!inp.length)
+                    inp = prompt('Input: ');
+
+                acc = inp.charCodeAt(0);
+            } else {
+                if (acc > 0)
+                    out += String
+                        .fromCharCode(acc - 1);
+            }
+
+            state = {
+                tape: tape,
+                acc: acc,
+                ind: ind,
+                ptr: ptr,
+                out: out,
+                end: end
+            };
+
+            arr.push(state);
+            return state;
+        };
+
+        return {
+            run: inner,
+            code: code
+        };
+    }
+
+    return run;
+}
+
+export default function Suffolk() {
+    let obj = {
         tape: [0],
         acc: 0,
         ind: 0,
@@ -9,89 +96,7 @@ let obj = {
         end: false
     };
 
-function run(input) {
-    let code = '';
-
-    for (let c of input)
-        if ('><!,.'.includes(c))
-            code += c;
-
-    let len = code.length;
-    let arr = [obj];
-    let num = 0;
-
-    function inner(back = false) {
-        let state = arr[arr.length - 1];
-        let c = code[num % len];
-        let {end} = state;
-
-        if (back) {
-            if (num)
-                num -= 1;
-        } else {
-            num += 1;
-
-            if (num % len === 0)
-                end = true;
-            else if (state.end)
-                end = false;
-        }
-
-        if (num < arr.length)
-            return arr[num];
-
-        let {tape, acc, ptr, out} = state;
-        let ind = num % len;
-        tape = [...tape];
-
-        if (c === '>') {
-            ptr += 1;
-            if (ptr === tape.length)
-                tape.push(0);
-        } else if (c === '<') {
-            acc += tape[ptr];
-            ptr = 0;
-        } else if (c === '!') {
-            tape[ptr] -= acc - 1;
-
-            if (tape[ptr] < 0)
-                tape[ptr] = 0;
-
-            acc = 0;
-            ptr = 0;
-        } else if (c === ',') {
-            let inp = '';
-
-            while (!inp.length)
-                inp = prompt('Input: ');
-
-            acc = inp.charCodeAt(0);
-        } else {
-            if (acc > 0)
-                out += String
-                    .fromCharCode(acc - 1);
-        }
-
-        state = {
-            tape: tape,
-            acc: acc,
-            ind: ind,
-            ptr: ptr,
-            out: out,
-            end: end
-        };
-
-        arr.push(state);
-        return state;
-    };
-
-    return {
-        run: inner,
-        code: code
-    };
-}
-
-export default function Suffolk() {
+    let run = outer(obj);
     return <TextBox
         name='Suffolk'
         start={obj}
