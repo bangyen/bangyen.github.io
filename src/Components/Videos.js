b3104e190d190bcfe6c7d32e24eac83fb82827f4
Bangyen Pham <bangyen99@gmail.com>
2022-05-20 23:09:17 -0400

    removed unnecessary props

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -9,14 +9,6 @@ function getVideo(embedId, name) {
             height="480"
             src={`https://www.youtube.com/embed/${embedId}`}
             frameBorder="0"
-            allow={`
-                accelerometer;
-                autoplay;
-                clipboard-write;
-                encrypted-media;
-                gyroscope;
-                picture-in-picture
-            `}
             allowFullScreen
             title={name}
         />
