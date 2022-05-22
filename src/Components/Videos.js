60edcafafec2a44d9d5f94bd23f213e0da6aa1d4
Bangyen Pham <bangyen99@gmail.com>
2022-05-21 23:06:59 -0400

    dynamically resize videos

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,11 +1,11 @@
 import {button, home} from './helper';
 import React from "react";
 
-function getVideo(embedId, name) {
+function getVideo(embedId, name, width) {
     return <div>
         <iframe
-            width="853"
-            height="480"
+            width={width}
+            height={width / 16 * 9}
             src={`https://www.youtube.com/embed/${embedId}`}
             frameBorder="0"
             allowFullScreen
@@ -25,16 +25,46 @@ export default class Videos extends React.Component {
             ['Firefly 2021', 'nnwVZDGj-SU']
         ];
 
-        this.change
-            = this.change.bind(this);
-        this.state = {num: 0};
+        this.changeNum
+            = this.changeNum.bind(this);
+        this.changeWidth
+            = this.changeWidth.bind(this);
+
+        this.state = {
+            width: this.getWidth(),
+            num: 0
+        };
     }
 
     componentDidMount() {
         document.title = 'Videos | Bangyen';
+        window.addEventListener(
+            'resize', this.changeWidth);
     }
 
-    change(val) {
+    componentWillUnmount() {
+        window.removeEventListener(
+            'resize', this.changeWidth);
+    }
+
+    getWidth() {
+        const height = window.innerHeight;
+        let width = window.innerWidth;
+
+        if (1.1 * height > width)
+            width = width * 0.9;
+        else
+            width = width / 2;
+
+        return width;
+    }
+
+    changeWidth() {
+        const width = this.getWidth();
+        this.setState({width});
+    }
+
+    changeNum(val) {
         const len = this.id.length;
         const {num} = this.state;
         val = num + val + len;
@@ -47,26 +77,26 @@ export default class Videos extends React.Component {
     }
 
     render() {
-        const {num} = this.state;
+        const {num, width} = this.state;
         const [name, id] = this.id[num];
 
         return (
             <header className='App-header'>
-                <h1>
+                <h1 style={{fontSize: 'min(10vh, 8vw)'}}>
                     <center>
                         <code>
                             {name}
                         </code>
                     </center>
                 </h1>
-                {getVideo(id, name)}
+                {getVideo(id, name, width)}
                 <div>
                     {button('❮',
-                        this.change(-1),
+                        this.changeNum(-1),
                         'Previous')}
                     {home()}
                     {button('❯',
-                        this.change(1),
+                        this.changeNum(1),
                         'Next')}
                 </div>
             </header>
