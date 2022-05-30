316b88a4236217a8a5e179b5d693f36fb4e803ee
Bangyen Pham <bangyen99@gmail.com>
2022-05-29 21:28:01 -0400

    Updates

--- src/Components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Home } from './Home';
-export { default as Error } from './Error';
-export { default as Snake } from './Snake';
-export { default as Snowman } from './Snowman';
-export { default as Videos } from './Videos';
-
-export const pages
-    = ['Snake', 'Snowman', 'Videos'];
