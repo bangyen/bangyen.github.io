8b10e9a4253fc0449eeb90eb65e5691dfb964a2e
Bangyen Pham <bangyen99@gmail.com>
2024-04-04 23:57:32 -0500

    removed bio, added resume

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -28,6 +28,7 @@ function dropdown(name, size, options) {
 }
 
 export default function Home() {
+    const pdfURL   = 'resume.pdf';
     document.title = 'Home | Bangyen';
 
     return (
@@ -49,63 +50,9 @@ export default function Home() {
             <h1 className='logo'>
                 Bangyen
             </h1>
-            <b>
-                <code style={{
-                    fontSize: '3vh'
-                }}>
-                    About Me
-                </code>
-            </b>
-            <center style={{
-                    fontSize: '2vh',
-                    width: '40%',
-                    paddingTop: '2vh',
-                    paddingBottom: '2vh'
-                }}>
-                <code>
-                    I am a first-year PhD student working
-                        with <a href="http://pdinda.org/">Peter Dinda</a> in
-                        the Prescience Lab at Northwestern University.
-                    I have a B.A. in Mathematics and Economics from the University of Virginia.
-                    I study HPC optimizations, specifically the use of machine learning to autotune MPI collective operations.
-                </code>
-            </center>
-            <b>
-                <code style={{
-                        fontSize: '3vh'
-                    }}>
-                    Contact Info
-                </code>
-            </b>
-            <center>
-                <code style={{
-                        fontSize: '2vh'
-                    }}>
-                    <ul style={{ textAlign: 'left' }}>
-                        <li>bangyen (at) northwestern (dot) edu</li>
-                        <li>Seeley G. Mudd Library, Room 3308</li>
-                    </ul>
-                </code>
-            </center>
-            <b>
-                <code style={{
-                        fontSize: '3vh'
-                    }}>
-                    Favorite Papers
-                </code>
-            </b>
-            <center style={{
-                fontSize: '2vh',
-                paddingBottom: '2vh'
-                }}>
-                <code>
-                    <ul style={{textAlign: 'left'}}>
-                        <li><a href="https://ieeexplore.ieee.org/document/9923793">RipTide: A Programmable, Energy-Minimal Dataflow Compiler and Architecture (G. Gobieski et al.)</a></li>
-                        <li><a href="https://dl.acm.org/doi/10.1145/349299.349342">ABCD: eliminating array bounds checks on demand (Rastislav Bodik, Rajiv Gupta, and Vivek Sarkar)</a></li>
-                        <li><a href="https://dl.acm.org/doi/10.1145/1379022.1375609">Register allocation by puzzle solving (Fernando Magno Quintao Pereira and Jens Palsberg)</a></li>
-                    </ul>
-                </code>
-            </center>
+            <div style={{display: 'none'}}>
+                <iframe src={pdfURL} width='1000vw' height='1300vh' />
+            </div>
         </header>
     );
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -2,12 +2,12 @@ export { default as Home } from './Home';
 export { default as Error } from './Error';
 export { default as Snake } from './Snake';
 export { default as Snowman } from './Snowman';
-export { default as Videos } from './Videos';
+// export { default as Videos } from './Videos';
 // export { default as Hotline } from './Hotline';
 
 export const pages = {
     Snake: '/Snake',
     Snowman: '/Snowman',
-    Videos: '/Videos',
+    // Videos: '/Videos',
     // Hotline: '/Hotline'
 };
