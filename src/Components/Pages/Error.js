c0392cdcfa07865ac7f9874fda80e166c304c67f
Bangyen Pham <bangyen99@gmail.com>
2024-09-08 00:04:45 -0500

    refactored error page

--- src/Components/Pages/Error.js
+++ src/Components/Pages/Error.js
@@ -1,20 +1,41 @@
-import {home} from '../oldHelpers';
 import React from 'react';
+import Grid from '@mui/material/Grid2';
+import { Typography } from '@mui/material';
+import { HomeButton } from '../helpers';
 
 export default function Error() {
-    document.title = 'Page Not Found | Bangyen';
+    document.title
+        = 'Page Not Found | Bangyen';
     return (
-        <header className='app'>
-            <center style={{
-                    fontSize: '5vh',
-                    paddingBottom: '2vh'}}>
-                <code>
+        <Grid
+            container
+            direction='column'
+            height='100vh'>
+            <Grid
+                flex={1}
+                display='flex'
+                justifyContent='center'
+                alignItems='center'>
+                    <Typography
+                        sx={{
+                        typography: {
+                            xs: 'h5',
+                            sm: 'h4',
+                            md: 'h3'
+                        }
+                    }}>
                     This page isn't available.
-                </code>
-            </center>
-            <div>
-                {home(400)}
-            </div>
-        </header>
+                </Typography>
+            </Grid>
+            <Grid
+                position='absolute'
+                bottom={30}
+                left='50%'
+                transform='translateX(-50%)'
+                display='flex'
+                justifyContent='center'>
+                <HomeButton />
+            </Grid>
+        </Grid>
     );
 }
