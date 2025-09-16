5dbdf570e9d1178425f17be1ab099144752c3656
Bangyen Pham <bangyen99@gmail.com>
2025-09-16 11:36:40 -0500

    Rename files to remove 'Demo' from filenames
    - Rename ZSharpDemo.js to ZSharp.js
    - Rename OligopolyDemo.js to Oligopoly.js
    - Update import statements in Pages/index.js to reference new filenames
    - Complete removal of 'Demo' from entire codebase
    - All naming now consistent: files, components, routes, and exports

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,8 +2,8 @@ export { default as Home } from './Home';
 export { default as Error } from './Error';
 export { default as Snake } from './Snake';
 export { default as Lights_Out } from './Lights/LightsOut';
-export { default as ZSharp } from './ZSharpDemo';
-export { default as Oligopoly } from './OligopolyDemo';
+export { default as ZSharp } from './ZSharp';
+export { default as Oligopoly } from './Oligopoly';
 
 export const pages = {
     Lights_Out: '/Lights_Out',
