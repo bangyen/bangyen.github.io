f3e7c3e4965be5d5495dee594d7549edbe4600c5
Bangyen Pham <bangyen99@gmail.com>
2026-01-24 21:35:21 -0600

    Hide research demo subtitles on mobile

--- src/features/research/ResearchDemo.tsx
+++ src/features/research/ResearchDemo.tsx
@@ -157,6 +157,7 @@ const ResearchDemo = <T,>({
                                     fontSize: TYPOGRAPHY.fontSize.subheading,
                                     textAlign: { xs: 'center', md: 'right' },
                                     whiteSpace: { xs: 'normal', md: 'nowrap' },
+                                    display: { xs: 'none', md: 'block' },
                                 }}
                             >
                                 {subtitle}
