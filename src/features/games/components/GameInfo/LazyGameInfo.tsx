2ef8b52715e4537575b162f30aec8d3bd4e1ca1c
Bangyen Pham <bangyen99@gmail.com>
2026-02-18 17:42:44 -0600

    feat: standardize verbiage to 'Try Again' in LazyGameInfo

--- src/features/games/components/GameInfo/LazyGameInfo.tsx
+++ src/features/games/components/GameInfo/LazyGameInfo.tsx
@@ -131,7 +131,7 @@ function ErrorContent({
                     onClick={resetErrorBoundary}
                     sx={errorButtonSx}
                 >
-                    Retry
+                    Try Again
                 </Button>
                 {handleClose && (
                     <Button
