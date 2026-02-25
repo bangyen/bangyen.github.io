67ba56dcffbffb41879152ca8d20cf1b7dc3a1af
Bangyen Pham <bangyen99@gmail.com>
2026-02-24 18:51:08 -0600

    refactor: remove unused useAsync and useResearchData hooks

--- src/features/research/hooks/index.ts
+++ src/features/research/hooks/index.ts
@@ -1,6 +1,6 @@
 export { useCurrentView } from './useCurrentView';
 export { useListNavigation } from './useListNavigation';
 export { useOligopolyState } from './useOligopolyState';
-export { useResearchData } from './useResearchData';
+
 export { resolveResearchDemoDefaults } from './useResearchDemoDefaults';
 export type { ResearchDemoDefaults } from './useResearchDemoDefaults';
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
-export { useAsync } from './useAsync';
-export type { UseAsyncResult } from './useAsync';
 export { useDebouncedEffect } from './useDebouncedEffect';
 export { useWindow, useMobile } from './layout';
 export { useLocalStorage } from './useLocalStorage';
