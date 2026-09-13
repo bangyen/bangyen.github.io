98278e45c372ae35452b2552673ec74f7d1aa722
Bangyen Pham <bangyen99@gmail.com>
2026-09-13 11:18:58 -0400

    tighten project menu width

--- src/components/layout/ProjectMenu/index.tsx
+++ src/components/layout/ProjectMenu/index.tsx
@@ -75,7 +75,8 @@ export function MenuButton(): React.ReactElement {
                     marginLeft: 1,
                     marginTop: 1,
                     '& .MuiPaper-root': {
-                        width: 'min(320px, calc(100vw - 32px))',
+                        width: 'fit-content',
+                        maxWidth: 'min(300px, calc(100vw - 32px))',
                         height: 'auto !important',
                         ...ANIMATIONS.presets.glassSoft,
                         borderRadius: SPACING.borderRadius.lg,
